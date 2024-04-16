0395d6e6423ad710edfca57708a449ce1a77804a
sniperlyf <sniperlyfe564@gmail.com.com>
2024-04-16 20:13:05 +0530

    02datatype.js config done

--- basic/02datatype.js
+++ basic/02datatype.js
@@ -1,1 +1,19 @@
 "use strict"; //treat all js code as new version
+
+//alert("hello") we are using node.js , not browser therfore not working
+
+console.log(3+3);
+console.log("krishna");
+
+console.table([3+3,"krishna"])
+
+let name="karan"
+let age =18
+let islogin =false
+
+//number =>2^53 is range of number datatype
+
+//bigint
+//string=> " "
+//boolean => true or false
+//
