d4db9e253aba12499df169df9a3ea03f3c2f64ce
sniperlyf <sniperlyfe564@gmail.com.com>
2024-04-16 19:43:24 +0530

    01basic.js config done

--- basic/01basic.js
+++ basic/01basic.js
@@ -1,8 +1,16 @@
-const accoundID = 124001
-let accoundEmail = "cosmoxmuthal@gmail.com"
-var accountPassword = "12345"
+const accountID = 124001
+let accountEmail = "cosmoxmuthal@gmail.com"
+var accountPassword = "12345"//always use "let" for initializing variable as "var" had issue with in block scope and functional scope
 accountCity = "Rohtak"
 
-accountID = 2 
+//accountID = 2   constant keyword can be changed that is not allowed
+console.log(accountID)
 
-console.log(accountID);
+accountEmail="hctch@gmail.com"
+accountPassword="12121"
+accountCity="jaipur"
+
+
+//consol method table that can be used to print multiple consol.log instead of writing it again n again
+ 
+console.table([accountEmail,accountID,accountPassword,accountCity])
