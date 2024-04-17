f442569c318e6dc7d989485654304eef5969fdb4
sniperlyf <sniperlyfe564@gmail.com.com>
2024-04-17 14:26:11 +0530

    03conversionoperations.js config done

---
+++ basic/03conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = null//returns 0;
+// let score = undefined//returns NaN;
+let score = true//returns 1;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" converts easily 33
+//"33abc" -> NaN
+//true -> 1 and false ->0
+
+
+let isLoggedIn = 1
+let booleanIsLOggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLOggedIn)//return true
+
+let stringNumber = 33
+console.log(stringNumber)
+console.log(typeof stringNumber)//returns number but now
+
+let someNumber = String(stringNumber)
+console.log(typeof someNumber)//returns string
+
