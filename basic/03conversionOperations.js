8a4cf999bd72a62d82fb69494511149edf23aa73
sniperlyf <sniperlyfe564@gmail.com.com>
2024-04-17 15:04:02 +0530

    03conversionOperations config done

--- basic/03conversionOperations.js
+++ basic/03conversionOperations.js
@@ -1,28 +1,59 @@
 // let score = null//returns 0;
 // let score = undefined//returns NaN;
-let score = true//returns 1;
+// let score = true//returns 1;
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
-
-
-// "33" converts easily 33
-//"33abc" -> NaN
-//true -> 1 and false ->0
+// let valueInNumber = Number(score)
+// //console.log(typeof valueInNumber);
+// //console.log(valueInNumber);
 
 
-let isLoggedIn = 1
-let booleanIsLOggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLOggedIn)//return true
+// // "33" converts easily 33
+// //"33abc" -> NaN
+// //true -> 1 and false ->0
 
-let stringNumber = 33
-console.log(stringNumber)
-console.log(typeof stringNumber)//returns number but now
 
-let someNumber = String(stringNumber)
-console.log(typeof someNumber)//returns string as answer
+// let isLoggedIn = 1
+// let booleanIsLOggedIn = Boolean(isLoggedIn)
+// //console.log(booleanIsLOggedIn)//return true
 
+// let stringNumber = 33
+// console.log(stringNumber)
+// console.log(typeof stringNumber)//returns number but now
+
+// let someNumber = String(stringNumber)
+// console.log(typeof someNumber)//returns string as answer
+
+//**********************OPERATIONS************************ */
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/2)
+// console.log(2%2)
+
+
+// let st1 = "hello"
+// let st2 = " krish"
+
+// let st3 = st1+st2
+// console.log(st3)
+
+
+// //problem occurs when
+// console.log("1"+2)
+// console.log(1+"2")//both return 12 why??
+// console.log(1+"2" + 2)//122
+// console.log(1 + 2 + "2")//32
+
+let game = 100
+//++game;prefix
+game++;//both return 101
+console.log(game)
